fbc8e08e24971891d4169d8d55d9251fa5fcd493
Krishab Bashyal <krishab2002@gmail.com>
2022-12-30 19:03:17 -0600

    Started working on the landing page of the app

---
+++ client/src/components/AboutApplication.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class='bg-blue'>
+    <p class='no-station'>No charging station?</p>
+    <p class='no-problem'>No Problem!</p>
+    <p>Using our network of individually listed vehicle chargers we aim to reduce the strain on the existing charging network and make it easier for people to charge their vehicles while on the go.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutApplication",
+};
+</script>
+
+<style scoped>
+.bg-blue{
+  background-color: #0066FF;
+  color: white ;
+  text-align: center;
+  padding: .8rem;
+  width: 100%;
+}
+.no-station{
+  font-size: 1.3rem;
+  font-weight: 700;
+  margin: 0;
+}
+
+.no-problem{
+  font-size: 1.3rem;
+  margin-bottom: .8rem;
+}
+</style>
+
+
---
+++ client/src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>ElectricEdge</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+}
+</script>
+
+<style scoped>
+
+h1{
+  color: #0066FF;
+  font-size: 1.7rem;
+  padding: 2.4rem;
+  font-style: normal;
+  font-weight: 800;
+  line-height: 2rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.301);
+}
+
+</style>
+
