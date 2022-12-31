b6d3e5e5121f997573ed08a6603732b9c412139b
Krishab Bashyal <krishab2002@gmail.com>
2022-12-31 01:07:37 -0600

    header more bold

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -17,7 +17,7 @@ h1{
   font-size: 1.55rem;
   padding: 2.4rem;
   font-style: normal;
-  font-weight: 800;
+  font-weight: 900;
   line-height: 2rem;
   border-bottom: 1px solid rgba(0, 0, 0, 0.301);
 }
