eec4e188c63958585088157d949bc747259133cd
AankitP <2001aankit@gmail.com>
2023-01-01 15:14:07 -0600

    Text
    Added trademark text that shows up after ElectricEdge in the footer

--- client/src/components/GlobalAppFooter.vue
+++ client/src/components/GlobalAppFooter.vue
@@ -4,60 +4,52 @@
             ElectricEdge
         </h1>
         <h2>
-            Terms and Conditions
-            Privacy Policy
+            Terms and Conditions  <br>
+            Privacy Policy  <br>
             Copyright © 2022
         </h2>
     </div>
 </template>
-  bet
-  <script>
-  export default {
-    name: 'AppHeader',
-  }
-  </script>
-  
-  <style scoped>
-  
-  .footer{
-    position: absolute;
-    background-color: #ffffff;
-    width: 150rem;
-    height: 10rem;
-    border-top: 1px solid rgba(0, 0, 0, 0.301);
-    background: linear-gradient(180deg, #090724 30.21%, #002B6C 100%);
-  }
-  
-  h1{
-    color: #FFFFFF;
-    /* text-align: center; */
-    font-size: 1.55rem;
-    padding: 2.4rem;
-    font-style: normal;
-    font-weight: 900;
-    line-height: 2rem;
-  }
 
-  h2{
-    position: absolute;
-    width: 174px;
-    height: 43px;
-    left: 91px;
-    top: 270px;
 
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 15px;
-    display: flex;
-    align-items: center;
-    text-align: center;
+<script>
 
-    color: #FFFFFF;
-  }
+    export default {
+        name: 'AppFooter',
+    }
 
+</script>
   
-  </style>
-  
-  
+<style scoped>
+
+    .footer{
+        background-color: #ffffff;
+        width: 150rem;
+        height: 7rem;
+        border-top: 1px solid rgba(0, 0, 0, 0.301);
+        background: linear-gradient(180deg, #090724 30.21%, #002B6C 100%);
+    }
+
+    h1{
+        color: #FFFFFF;
+        text-align: center;
+        font-size: 1.55rem;
+        padding-top: 1rem;
+        font-style: normal;
+        font-weight: 900;
+        line-height: 2rem;
+    }
+
+    h2{
+        text-align: center;
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 15px;
+        color: #FFFFFF;
+    }
+
+
+</style>
+
