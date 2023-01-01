9c1653155929c32d88da2a57224541876a4e2109
AankitP <2001aankit@gmail.com>
2023-01-01 16:10:56 -0600

    Made Requested Changes
    Changed <h2> from GlobalAppFooter.vue to <p>
    Increased spacing between the footer lines

--- client/src/components/GlobalAppFooter.vue
+++ client/src/components/GlobalAppFooter.vue
@@ -1,55 +1,43 @@
 <template>
-    <div class='footer'>
-        <h1>
-            ElectricEdge
-        </h1>
-        <h2>
-            Terms and Conditions  <br>
-            Privacy Policy  <br>
-            Copyright © 2023
-        </h2>
-    </div>
+  <div class="footer">
+    <h1>ElectricEdge</h1>
+    <p>Terms and Conditions</p>
+    <p>Privacy Policy</p>
+    <p>Copyright © 2023</p>
+  </div>
 </template>
 
-
 <script>
-
-    export default {
-        name: 'AppFooter',
-    }
-
+export default {
+  name: "AppFooter",
+};
 </script>
-  
+
 <style scoped>
+.footer {
+  background-color: #ffffff;
+  width: calc(100%);
+  height: 7rem;
+  border-top: 1px solid rgba(0, 0, 0, 0.301);
+  background: linear-gradient(180deg, #090724 30.21%, #002b6c 100%);
+}
 
-    .footer{
-        background-color: #ffffff;
-        width: calc(100%);
-        height: 7rem;
-        border-top: 1px solid rgba(0, 0, 0, 0.301);
-        background: linear-gradient(180deg, #090724 30.21%, #002B6C 100%);
-    }
+h1 {
+  color: #ffffff;
+  text-align: center;
+  font-size: 1.55rem;
+  padding-top: 1rem;
+  font-style: normal;
+  font-weight: 900;
+  line-height: 2rem;
+}
 
-    h1{
-        color: #FFFFFF;
-        text-align: center;
-        font-size: 1.55rem;
-        padding-top: 1rem;
-        font-style: normal;
-        font-weight: 900;
-        line-height: 2rem;
-    }
-
-    h2{
-        text-align: center;
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 12px;
-        line-height: 15px;
-        color: #FFFFFF;
-    }
-
-
+p {
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.8rem;
+  line-height: 1.1rem;
+  color: #ffffff;
+}
 </style>
-
