708de62e555f6ac8207db65d85c6c48cff9fcc2b
Krishab Bashyal <krishab2002@gmail.com>
2023-01-01 18:11:12 -0600

    Added responiveness to the header/navbar cleaned up assets file

--- client/src/components/HeroSection.vue
+++ client/src/components/HeroSection.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>We are changing the charging game.</h1>
     <p>The easiest way to find chargers on the go.</p>
-    <img src="../assets/ElectricEdgeHero.png" alt="House with solar panels charging a blue electric car" />
+    <img src="../assets/images/ElectricEdgeHero.png" alt="House with solar panels charging a blue electric car" />
   </div>
 </template>
 
