6eab32cdc07eca3bded78313e49871f51ebc05df
Krishab Bashyal <krishab2002@gmail.com>
2023-02-08 17:13:21 -0600

    Finally got something working for the footer

--- client/src/components/LoadingSpinner.vue
+++ client/src/components/LoadingSpinner.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="loadingContainer">
-    <h1 class="loadingText">Loading...</h1>
-    <p>Just a couple more seconds!</p>
+    <div class="contentFloat">
+      <h1 class="loadingText">Loading...</h1>
+      <p>Just a couple more seconds!</p>
+    </div>
   </div>
     
 </template>
@@ -19,5 +21,12 @@ export default {
 <style scoped>
 .loadingContainer{
   text-align: center;
+  height: calc(100vh - 13.30rem); /* SECOND VALUE IN THIS LINE IS THE MARGIN + PADDING ON THE HEADER AND FOOTER */
+}
+
+.contentFloat{
+  margin-top: 3rem;
+  text-align: center;
+  position: absolute;
 }
 </style>
