3342d76cd192b20d999a7bc91bb02f176ef5a6cf
Krishab Bashyal <krishab2002@gmail.com>
2023-02-08 12:42:25 -0600

    Created a route for the landing page

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import LandingPage from './pages/LandingPage'
+
+
+const app = createApp(App)
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: LandingPage},
+    ]
+})
+
+app.use(router)
+app.mount('#app')
