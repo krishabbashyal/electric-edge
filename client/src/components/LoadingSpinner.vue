b6344f25a6076f879089ae2be97c9373468f1aee
Krishab Bashyal <krishab2002@gmail.com>
2023-02-08 18:03:02 -0600

    Added loading spinner

--- client/src/components/LoadingSpinner.vue
+++ client/src/components/LoadingSpinner.vue
@@ -1,32 +1,78 @@
 <template>
   <div class="loadingContainer">
     <div class="contentFloat">
-      <h1 class="loadingText">Loading...</h1>
-      <p>Just a couple more seconds!</p>
+      <h1 class="loadingText">Loading</h1>
+      <p>This should not take long</p>
+      <div class="animation">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   </div>
-    
 </template>
 
 <script>
-
-
-
 export default {
-    name: "LoadingSpinner",
-
-}
+  name: "LoadingSpinner",
+};
 </script>
 
 <style scoped>
-.loadingContainer{
+.loadingContainer {
   text-align: center;
-  height: calc(100vh - 13.30rem); /* SECOND VALUE IN THIS LINE IS THE MARGIN + PADDING ON THE HEADER AND FOOTER */
+  color: black;
+  font-size: 0.75rem;
+  height: calc(100vh - 13.3rem);
+  /* SECOND VALUE IN THIS LINE IS THE MARGIN + PADDING ON THE HEADER AND FOOTER */
 }
 
-.contentFloat{
-  margin-top: 3rem;
-  text-align: center;
-  position: absolute;
+p {
+  color: #0066ff;
 }
+
+.contentFloat {
+  margin-top: 14rem;
+  position: absolute;
+  left: 0;
+  right: 0;
+}
+
+.animation {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.animation div {
+  display: inline-block;
+  position: absolute;
+  left: 8px;
+  width: 12px;
+  background: #0066FF;
+  animation: animation 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+}
+.animation div:nth-child(1) {
+  left: 8px;
+  animation-delay: -0.24s;
+}
+.animation div:nth-child(2) {
+  left: 32px;
+  animation-delay: -0.12s;
+}
+.animation div:nth-child(3) {
+  left: 56px;
+  animation-delay: 0;
+}
+@keyframes animation {
+  0% {
+    top: 8px;
+    height: 64px;
+  }
+  50%, 100% {
+    top: 24px;
+    height: 32px;
+  }
+}
+
 </style>
